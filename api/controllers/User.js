33cb45e075266bc6daa63c9f1883f9df01dfcabd
Ivanlpc <100932340+Ivanlpc@users.noreply.github.com>
2023-09-22 13:36:07 +0200

    change bot name

--- api/controllers/User.js
+++ api/controllers/User.js
@@ -5,7 +5,7 @@ const Logger = require('../../util/Logger')
 const getNameByUUID = async (uuid) => {
   if (uuid === null) return ''
   try {
-    const query = await execute(QUERIES.GetLastNickname, [uuid])
+    const query = await execute(QUERIES.getLastNickname, [uuid])
     return query[0].lastNickname
   } catch (err) {
     Logger.error(err)
@@ -16,7 +16,7 @@ const getNameByUUID = async (uuid) => {
 const checkPermission = async (id, permissionId, guildId) => {
   if (id === null) return false
   try {
-    const getPermissions = await execute(QUERIES.GetPermissions, [id, permissionId, guildId])
+    const getPermissions = await execute(QUERIES.getPermissions, [id, permissionId, guildId])
     return getPermissions.length > 0
   } catch (err) {
     Logger.error(err)
@@ -24,7 +24,65 @@ const checkPermission = async (id, permissionId, guildId) => {
   }
 }
 
+const addPermission = async (id, permissionId, guildId) => {
+  try {
+    const query = await execute(QUERIES.addPermission, [guildId, id, permissionId])
+    return query.affectedRows > 0
+  } catch (err) {
+    Logger.error(err)
+    throw new Error('There was an error while trying to add the permission')
+  }
+}
+
+const removePermission = async (id, permissionId, guildId) => {
+  try {
+    const query = await execute(QUERIES.removePermission, [guildId, id, permissionId])
+    return query.affectedRows > 0
+  } catch (err) {
+    Logger.error(err)
+    throw new Error('There was an error while trying to remove the permission')
+  }
+}
+
+const getAllowedIds = async (guildId, permissionId) => {
+  let ids = []
+  try {
+    ids = await execute(QUERIES.getAllowedIds, [guildId, permissionId])
+    return ids.map(elem => elem.id)
+  } catch (err) {
+    Logger.error(err)
+    throw new Error('There was an error while trying to fetch the IDs ' + guildId + ' ' + permissionId)
+  }
+}
+
+const getIDPermissions = async (guildId, id) => {
+  let permissions = []
+  try {
+    permissions = await execute(QUERIES.getPermissionsOfId, [guildId, id])
+    return permissions.map(elem => elem.permission_node)
+  } catch (err) {
+    Logger.error(err)
+    throw new Error('There was an error while trying to fetch the IDs ' + guildId + ' ' + id)
+  }
+}
+
+const getNodeIds = async (guildId, permissionNode) => {
+  let ids = []
+  try {
+    ids = await execute(QUERIES.getPermissionsNode, [guildId, permissionNode])
+    return ids
+  } catch (err) {
+    Logger.error(err)
+    throw new Error('There was an error while trying to fetch the IDs ' + guildId + ' ' + permissionNode)
+  }
+}
+
 module.exports = {
   getNameByUUID,
-  checkPermission
+  checkPermission,
+  addPermission,
+  removePermission,
+  getAllowedIds,
+  getIDPermissions,
+  getNodeIds
 }
