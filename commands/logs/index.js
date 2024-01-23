de712aabe21044f5bae6a652b37bea84bfc13066
Ivanlpc <100932340+Ivanlpc@users.noreply.github.com>
2024-01-23 13:58:17 +0100

    logs command

---
+++ commands/logs/index.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const { SlashCommandBuilder } = require('discord.js')
+const command = require('../../config/config.json').commands.logs
+
+module.exports = {
+  permission: command.requires_permission ? command.name : '',
+  cooldown: command.cooldown || 0,
+  enabled: command.enabled,
+  data: new SlashCommandBuilder()
+    .setName(command.name)
+    .setDescription(command.description),
+  async execute (interaction) {
+    interaction.reply({
+      files: [path.join(__dirname, '../../logs/latest.txt')],
+      ephemeral: true
+    })
+  }
+
+}
