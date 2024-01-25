5d178b759dd2dbc3744931dec3fefe951d5b7161
Ivanlpc <100932340+Ivanlpc@users.noreply.github.com>
2024-01-25 10:52:02 +0100

    register commands in multiple guilds

--- CommandManager.js
+++ CommandManager.js
@@ -3,19 +3,21 @@ const path = require('path')
 const { REST, Routes } = require('discord.js')
 const TOKEN = process.env.TOKEN
 const CLIENT_ID = process.env.CLIENT_ID
-const GUILD_ID = process.env.GUILD_ID
 const ConsoleLogger = require('./util/ConsoleLogger')
 const COLOR = require('./util/ConsoleColor')
 const rest = new REST().setToken(TOKEN)
+const allowedGuilds = require('./config/config.json').allowed_guilds
 
 class CommandManager {
   static async registerCommands (commands) {
     try {
       ConsoleLogger.info(`${COLOR.WHITE}Registering commands...`)
-      const data = await rest.put(
-        Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-        { body: commands })
-      ConsoleLogger.info(`${COLOR.WHITE}Registered ${COLOR.GREEN}${data.length} ${COLOR.WHITE}commands`)
+      for (const guildId of allowedGuilds) {
+        const data = await rest.put(
+          Routes.applicationGuildCommands(CLIENT_ID, guildId),
+          { body: commands })
+        ConsoleLogger.info(`${COLOR.WHITE}Registered ${COLOR.GREEN}${data.length} ${COLOR.WHITE}commands in guild ${guildId}`)
+      }
     } catch (error) {
       ConsoleLogger.error(error)
     }
